01989ccedba95770531b0c789f0845a14ef08fe3
zihe.liu <ziheliu1024@gmail.com>
2024-04-11 20:31:58 +0800

    bitmap
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/storage/column_predicate.h
+++ be/src/storage/column_predicate.h
@@ -158,6 +158,8 @@ public:
     // Return false to filter out a data page.
     virtual bool bloom_filter(const BloomFilter* bf) const { return true; }
 
+    virtual bool support_bitmap_filter() const { return false; }
+
     [[nodiscard]] virtual Status seek_bitmap_dictionary(BitmapIndexIterator* iter, SparseRange<>* range) const {
         return Status::Cancelled("not implemented");
     }
