455ca1b421b43653cebff89a96dbb2d281dc8219
zihe.liu <ziheliu1024@gmail.com>
2024-11-14 19:15:30 +0800

    wip
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -780,6 +780,12 @@ public class Config extends ConfigBase {
     public static int rpc_port = 9020;
 
     /**
+     * Arrow Flight SQL server port
+     */
+    @ConfField
+    public static int arrow_flight_port = 9408;
+
+    /**
      * The connection timeout and socket timeout config for thrift server
      * The value for thrift_client_timeout_ms is set to be larger than zero to prevent
      * some hang up problems in java.net.SocketInputStream.socketRead0
@@ -3301,4 +3307,11 @@ public class Config extends ConfigBase {
     public static int batch_write_idle_ms = 3600000;
 
     public static int batch_write_executor_threads_num = 4096;
+
+    @ConfField(mutable = true)
+    public static int arrow_token_cache_size = 1024;
+
+    @ConfField(mutable = true)
+    public static int arrow_token_cache_expire = 3600;
+
 }
