5e4284738b9eab53c0f5a6f2fb85c9c15fd4329e
zihe.liu <ziheliu1024@gmail.com>
2024-11-26 17:48:17 +0800

    fix
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exprs/in_const_predicate.hpp
+++ be/src/exprs/in_const_predicate.hpp
@@ -288,7 +288,7 @@ public:
     }
 
     StatusOr<ColumnPtr> evaluate_with_filter(ExprContext* context, Chunk* ptr, uint8_t* filter) override {
-        const bool reselect = _is_join_runtime_filter && (++_evaluate_times) % 32 == 0;
+        const bool reselect = _is_join_runtime_filter && (_evaluate_times++) % 32 == 0;
         if (!reselect && !_used) {
             if (filter == nullptr) {
                 return RunTimeColumnType<TYPE_BOOLEAN>::create(ptr->num_rows(), true);
