9b6ddef918a810264d5b2f0c9dec2723d2e770b0
zihe.liu <ziheliu1024@gmail.com>
2024-12-18 16:14:20 +0800

    opt: eliminate virtual function for serialize_batch_with_null_masks
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/column/column.h
+++ be/src/column/column.h
@@ -497,6 +497,28 @@ public:
     Status accept_mutable(ColumnVisitorMutable* visitor) override {
         return visitor->visit(static_cast<Derived*>(this));
     }
+
+    void serialize_batch_with_null_masks(uint8_t* dst, Buffer<uint32_t>& slice_sizes, size_t chunk_size,
+                                         uint32_t max_one_row_size, uint8_t* null_masks, bool has_null) override {
+        uint32_t* sizes = slice_sizes.data();
+
+        if (!has_null) {
+            for (size_t i = 0; i < chunk_size; ++i) {
+                memcpy(dst + i * max_one_row_size + sizes[i], &has_null, sizeof(bool));
+                sizes[i] += static_cast<uint32_t>(sizeof(bool)) +
+                            mutable_derived()->serialize(i, dst + i * max_one_row_size + sizes[i] + sizeof(bool));
+            }
+        } else {
+            for (size_t i = 0; i < chunk_size; ++i) {
+                memcpy(dst + i * max_one_row_size + sizes[i], null_masks + i, sizeof(bool));
+                sizes[i] += sizeof(bool);
+
+                if (!null_masks[i]) {
+                    sizes[i] += mutable_derived()->serialize(i, dst + i * max_one_row_size + sizes[i]);
+                }
+            }
+        }
+    }
 };
 
 } // namespace starrocks
