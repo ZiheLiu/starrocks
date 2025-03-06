3ad0d791fd4fb9537778416dabdb37eca6af8546
zihe.liu <ziheliu1024@gmail.com>
2025-03-06 14:07:59 +0800

    wip
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exec/join_hash_map.cpp
+++ be/src/exec/join_hash_map.cpp
@@ -580,7 +580,7 @@ Status JoinHashTable::build(RuntimeState* state) {
 }
 
 void JoinHashTable::reset_probe_state(starrocks::RuntimeState* state) {
-    _hash_map_type = _choose_join_hash_map();
+    // _hash_map_type = _choose_join_hash_map();
     switch (_hash_map_type) {
 #define M(NAME)                                                                                                       \
     case JoinHashMapType::NAME:                                                                                       \
