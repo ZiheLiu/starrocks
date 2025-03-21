26ad5598420e85f8859ecc2dc05fed052c6013b7
zihe.liu <ziheliu1024@gmail.com>
2025-03-21 13:46:22 +0800

    fix
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exprs/runtime_filter.cpp
+++ be/src/exprs/runtime_filter.cpp
@@ -434,9 +434,9 @@ void RuntimeBitsetFilter<LT>::_evaluate_vectorized(const Column* __restrict inpu
         const auto* values = GetContainer<LT>::get_data(nullable_column->data_column()).data();
         if (nullable_column->has_null()) {
             const uint8_t* null_data = nullable_column->immutable_null_column_data().data();
-            _bitset.template contains_batch<false /*CheckRange*/>(selection, values, null_data, from, to);
+            _bitset.template contains_batch<false /*CheckRange*/, null_is_true>(selection, values, null_data, from, to);
         } else {
-            _bitset.template contains_batch<false /*CheckRange*/>(selection, values, from, to);
+            _bitset.template contains_batch<false /*CheckRange*/, null_is_true>(selection, values, from, to);
         }
     } else {
         const auto* values = GetContainer<LT>::get_data(input_column).data();
