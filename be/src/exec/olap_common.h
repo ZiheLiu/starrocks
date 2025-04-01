4b7bad6afc33edfbef4ab5781c380adf67681d21
zihe.liu <ziheliu1024@gmail.com>
2025-04-01 21:13:38 +0800

    fix
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exec/olap_common.h
+++ be/src/exec/olap_common.h
@@ -214,9 +214,7 @@ using ColumnValueRangeType =  std::variant<
         ColumnValueRange<int32_t>,
         ColumnValueRange<int64_t>,
         ColumnValueRange<__int128>,
-        ColumnValueRange<StringValue>, // TODO: remove
         ColumnValueRange<Slice>,
-        ColumnValueRange<DateTimeValue>,
         ColumnValueRange<DecimalV2Value>,
         ColumnValueRange<bool>,
         ColumnValueRange<DateValue>,
