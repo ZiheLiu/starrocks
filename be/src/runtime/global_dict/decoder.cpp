c7572a39b6e8e6fd66d62d57657be24939db8a89
zihe.liu <ziheliu1024@gmail.com>
2025-04-02 16:40:57 +0800

    fix set_has_null
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/runtime/global_dict/decoder.cpp
+++ be/src/runtime/global_dict/decoder.cpp
@@ -159,6 +159,7 @@ Status GlobalDictDecoderBase<Dict>::decode_string(const Column* in, Column* __re
     res_data_column->append_strings(res_slices.data(), num_rows);
     strings::memcpy_inlined(res_column->null_column_data().data(), src_is_nulls,
                             num_rows * sizeof(NullColumn::ValueType));
+    res_column->set_has_null(column->has_null());
 
     return Status::OK();
 }
