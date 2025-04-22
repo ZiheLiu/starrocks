22d598f577b4922d49d6553b6bddc520b3683de3
zihe.liu <ziheliu1024@gmail.com>
2025-04-22 10:44:17 +0800

    fix
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exec/pipeline/query_context.cpp
+++ be/src/exec/pipeline/query_context.cpp
@@ -475,8 +475,8 @@ StatusOr<QueryContext*> QueryContextManager::get_or_register(const TUniqueId& qu
             // lookup query context for the second chance in sc_map
             if (sc_it != sc_map.end()) {
                 auto ctx = std::move(sc_it->second);
-                sc_map.erase(sc_it);
                 RETURN_CANCELLED_STATUS_IF_CTX_CANCELLED(ctx);
+                sc_map.erase(sc_it);
                 auto* raw_ctx_ptr = ctx.get();
                 context_map.emplace(query_id, std::move(ctx));
 
