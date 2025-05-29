2ca8f5860edbb2379cbec636f62874a5fddaeae5
zihe.liu <ziheliu1024@gmail.com>
2025-05-29 11:02:13 +0800

    [0529] try simliar to back
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/formats/parquet/encoding_plain.h
+++ be/src/formats/parquet/encoding_plain.h
@@ -253,7 +253,7 @@ public:
             return Status::OK();
         }
 
-        if (filter != nullptr) {
+        auto process = [&]<bool with_filter>() {
             std::vector<Slice> slices(count);
 
             bool has_non_null = false;
@@ -272,70 +272,85 @@ public:
                     continue;
                 }
 
-                int i = start_idx;
-
+                if constexpr (with_filter) {
+                    int i = start_idx;
 #ifdef __AVX2__
-                constexpr int W = 256 / (8 * sizeof(uint8_t));
-                const __m256i all0 = _mm256_setzero_si256();
-                uint32_t vlength[W];
-                size_t voffset[W];
-                for (; i + W <= idx; i += W) {
-                    const auto start_offset = _offset;
-                    for (int j = 0; j < W; j++) {
+                    constexpr int W = 256 / (8 * sizeof(uint8_t));
+                    const __m256i all0 = _mm256_setzero_si256();
+                    uint32_t vlength[W];
+                    size_t voffset[W];
+                    for (; i + W <= idx; i += W) {
+                        const auto start_offset = _offset;
+                        for (int j = 0; j < W; j++) {
+                            if (_offset >= _data.size) {
+                                return Status::InternalError(strings::Substitute(
+                                        "going to read out-of-bounds data, offset=$0,count=$1,size=$2", _offset, count,
+                                        _data.size));
+                            }
+                            const uint32_t length =
+                                    decode_fixed32_le(reinterpret_cast<const uint8_t*>(_data.data) + _offset);
+                            vlength[j] = length;
+                            _offset += sizeof(int32_t) + length;
+                        }
+
+                        __m256i vfilter = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(filter + i));
+                        const int used_mask = _mm256_movemask_epi8(_mm256_cmpgt_epi8(vfilter, all0));
+                        if (used_mask == 0) {                  // all not used.
+                        } else if (used_mask == 0xffff'ffff) { // all used.
+                            has_non_null = true;
+
+                            voffset[0] = sizeof(int32_t);
+                            for (int j = 1; j < W; j++) {
+                                voffset[j] = voffset[j - 1] + vlength[j - 1] + sizeof(int32_t);
+                            }
+
+                            for (int j = 0; j < W; j++) {
+                                slices[i + j] = Slice(_data.data + start_offset + voffset[j], vlength[j]);
+                            }
+                        } else {
+                            has_non_null = true;
+
+                            voffset[0] = sizeof(int32_t);
+                            for (int j = 1; j < W; j++) {
+                                voffset[j] = voffset[j - 1] + vlength[j - 1] + sizeof(int32_t);
+                            }
+
+                            phmap::priv::BitMask<uint32_t, 32> bitmask(used_mask);
+                            for (auto j : bitmask) {
+                                slices[i + j] = Slice(_data.data + start_offset + voffset[j], vlength[j]);
+                            }
+                        }
+                    }
+
+                    for (; i < idx; i++) {
                         if (_offset >= _data.size) {
                             return Status::InternalError(
                                     strings::Substitute("going to read out-of-bounds data, offset=$0,count=$1,size=$2",
                                                         _offset, count, _data.size));
                         }
+
                         const uint32_t length =
                                 decode_fixed32_le(reinterpret_cast<const uint8_t*>(_data.data) + _offset);
-                        vlength[j] = length;
+                        if (filter[i]) {
+                            has_non_null = true;
+                            slices[i] = Slice(_data.data + _offset + sizeof(int32_t), length);
+                        }
                         _offset += sizeof(int32_t) + length;
                     }
-
-                    __m256i vfilter = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(filter + i));
-                    const int used_mask = _mm256_movemask_epi8(_mm256_cmpgt_epi8(vfilter, all0));
-                    if (used_mask == 0) {                  // all not used.
-                    } else if (used_mask == 0xffff'ffff) { // all used.
-                        has_non_null = true;
-
-                        voffset[0] = sizeof(int32_t);
-                        for (int j = 1; j < W; j++) {
-                            voffset[j] = voffset[j - 1] + vlength[j - 1] + sizeof(int32_t);
-                        }
-
-                        for (int j = 0; j < W; j++) {
-                            slices[i + j] = Slice(_data.data + start_offset + voffset[j], vlength[j]);
-                        }
-                    } else {
-                        has_non_null = true;
-
-                        voffset[0] = sizeof(int32_t);
-                        for (int j = 1; j < W; j++) {
-                            voffset[j] = voffset[j - 1] + vlength[j - 1] + sizeof(int32_t);
-                        }
-
-                        phmap::priv::BitMask<uint32_t, 32> bitmask(used_mask);
-                        for (auto j : bitmask) {
-                            slices[i + j] = Slice(_data.data + start_offset + voffset[j], vlength[j]);
-                        }
-                    }
-                }
 #endif
+                } else {
+                    for (int i = start_idx; i < idx; i++) {
+                        if (_offset >= _data.size) {
+                            return Status::InternalError(
+                                    strings::Substitute("going to read out-of-bounds data, offset=$0,count=$1,size=$2",
+                                                        _offset, count, _data.size));
+                        }
 
-                for (; i < idx; i++) {
-                    if (_offset >= _data.size) {
-                        return Status::InternalError(
-                                strings::Substitute("going to read out-of-bounds data, offset=$0,count=$1,size=$2",
-                                                    _offset, count, _data.size));
-                    }
-
-                    const uint32_t length = decode_fixed32_le(reinterpret_cast<const uint8_t*>(_data.data) + _offset);
-                    if (filter[i]) {
-                        has_non_null = true;
+                        const uint32_t length =
+                                decode_fixed32_le(reinterpret_cast<const uint8_t*>(_data.data) + _offset);
                         slices[i] = Slice(_data.data + _offset + sizeof(int32_t), length);
+                        _offset += sizeof(int32_t) + length;
                     }
-                    _offset += sizeof(int32_t) + length;
                 }
             }
 
@@ -344,41 +359,15 @@ public:
             } else {
                 ColumnHelper::get_binary_column(dst)->append_default(count);
             }
+
+            return Status::OK();
+        };
+
+        if (filter == nullptr) {
+            return process.operator()<false>();
         } else {
-            std::vector<Slice> slices(count);
-
-            size_t idx = 0;
-            while (idx < count) {
-                const size_t start_idx = idx;
-                const bool is_null = is_nulls[idx++];
-
-                size_t run = 1;
-                while (idx < count && is_nulls[idx] == is_null) {
-                    idx++;
-                    run++;
-                }
-
-                if (is_null) {
-                    continue;
-                }
-
-                for (int i = start_idx; i < idx; i++) {
-                    if (_offset >= _data.size) {
-                        return Status::InternalError(
-                                strings::Substitute("going to read out-of-bounds data, offset=$0,count=$1,size=$2",
-                                                    _offset, count, _data.size));
-                    }
-
-                    const uint32_t length = decode_fixed32_le(reinterpret_cast<const uint8_t*>(_data.data) + _offset);
-                    slices[i] = Slice(_data.data + _offset + sizeof(int32_t), length);
-                    _offset += sizeof(int32_t) + length;
-                }
-            }
-
-            ColumnHelper::get_binary_column(dst)->append_strings(slices.data(), count);
+            return process.operator()<true>();
         }
-
-        return Status::OK();
     }
 
     Status skip(size_t values_to_skip) override {
