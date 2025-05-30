265c5b293df2dd0adb88fce1de22080db784de1c
zihe.liu <ziheliu1024@gmail.com>
2025-05-30 15:19:26 +0800

    reserve
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/formats/parquet/scalar_column_reader.cpp
+++ be/src/formats/parquet/scalar_column_reader.cpp
@@ -507,6 +507,7 @@ Status ScalarColumnReader::_read_range_impl(const Range<uint64_t>& range, const 
     } else {
         if (!_converter->need_convert) {
             SCOPED_RAW_TIMER(&_opts.stats->column_read_ns);
+            dst->reserve(dst->size() + range.span_size());
             return _reader->read_range(range, filter, content_type, dst.get());
         } else {
             if (_tmp_intermediate_column == nullptr) {
