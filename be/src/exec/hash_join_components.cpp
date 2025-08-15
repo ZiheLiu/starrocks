bfda1125ed6c80670d9c19c904fc7e0e599eedf5
zihe.liu <ziheliu1024@gmail.com>
2025-08-15 13:02:03 +0800

    do not re-decide in build
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exec/hash_join_components.cpp
+++ be/src/exec/hash_join_components.cpp
@@ -936,42 +936,42 @@ Status AdaptivePartitionHashJoinBuilder::build(RuntimeState* state) {
         }
     }
 
-    if (_partition_num > 1) {
-        std::string rows;
-        for (auto& builder : _builders) {
-            rows += std::to_string(builder->hash_table_row_count()) + ", ";
-        }
-        VLOG_OPERATOR << "TRACE: build "
-                      << "[rows=" << rows << "] "
-                      << "[total_rows=" << hash_table_row_count() << "] ";
-
-        const size_t total_num_rows = hash_table_row_count();
-        size_t l2_benefit = 0;
-        size_t l3_benefit = 0;
-        for (auto& builder : _builders) {
-            const size_t partition_num_rows = builder->hash_table_row_count();
-            if (partition_num_rows == 0) {
-                continue;
-            }
-
-            const double hit_l2_cache_rate =
-                    std::min(1.0, _L2_cache_size / static_cast<double>(_hash_table_bytes_per_row * partition_num_rows));
-            l2_benefit += _l2_benefit * hit_l2_cache_rate * partition_num_rows / total_num_rows;
-
-            const double hit_l3_cache_rate = std::min(
-                    1.0, _L3_cache_size / static_cast<double>(_hash_table_bytes_per_row * partition_num_rows / 2));
-            l3_benefit += _l3_benefit * hit_l3_cache_rate * partition_num_rows / total_num_rows;
-        }
-
-        if (l2_benefit < _probe_row_shuffle_cost && l3_benefit < _probe_row_shuffle_cost) {
-            VLOG_OPERATOR << "TRACE: build finds cannot benefit from partitioned hash join "
-                          << "[l2_benefit=" << l2_benefit << "] "
-                          << "[l3_benefit=" << l3_benefit << "] "
-                          << "[probe_row_shuffle_cost=" << _probe_row_shuffle_cost << "] ";
-            // No benefit from partitioned hash join.
-            RETURN_IF_ERROR(_convert_to_single_partition(state));
-        }
-    }
+    // if (_partition_num > 1) {
+    //     std::string rows;
+    //     for (auto& builder : _builders) {
+    //         rows += std::to_string(builder->hash_table_row_count()) + ", ";
+    //     }
+    //     VLOG_OPERATOR << "TRACE: build "
+    //                   << "[rows=" << rows << "] "
+    //                   << "[total_rows=" << hash_table_row_count() << "] ";
+    //
+    //     const size_t total_num_rows = hash_table_row_count();
+    //     size_t l2_benefit = 0;
+    //     size_t l3_benefit = 0;
+    //     for (auto& builder : _builders) {
+    //         const size_t partition_num_rows = builder->hash_table_row_count();
+    //         if (partition_num_rows == 0) {
+    //             continue;
+    //         }
+    //
+    //         const double hit_l2_cache_rate =
+    //                 std::min(1.0, _L2_cache_size / static_cast<double>(_hash_table_bytes_per_row * partition_num_rows));
+    //         l2_benefit += _l2_benefit * hit_l2_cache_rate * partition_num_rows / total_num_rows;
+    //
+    //         const double hit_l3_cache_rate = std::min(
+    //                 1.0, _L3_cache_size / static_cast<double>(_hash_table_bytes_per_row * partition_num_rows / 2));
+    //         l3_benefit += _l3_benefit * hit_l3_cache_rate * partition_num_rows / total_num_rows;
+    //     }
+    //
+    //     if (l2_benefit < _probe_row_shuffle_cost && l3_benefit < _probe_row_shuffle_cost) {
+    //         VLOG_OPERATOR << "TRACE: build finds cannot benefit from partitioned hash join "
+    //                       << "[l2_benefit=" << l2_benefit << "] "
+    //                       << "[l3_benefit=" << l3_benefit << "] "
+    //                       << "[probe_row_shuffle_cost=" << _probe_row_shuffle_cost << "] ";
+    //         // No benefit from partitioned hash join.
+    //         RETURN_IF_ERROR(_convert_to_single_partition(state));
+    //     }
+    // }
 
     for (auto& builder : _builders) {
         RETURN_IF_ERROR(builder->build(state));
