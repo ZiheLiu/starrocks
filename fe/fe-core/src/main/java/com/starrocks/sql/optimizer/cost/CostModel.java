ed67dc9474dc084c7a626af20c36ec70410365e6
zihe.liu <ziheliu1024@gmail.com>
2025-09-16 16:33:23 +0800

    fix costModel for low card
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/cost/CostModel.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/cost/CostModel.java
@@ -355,12 +355,9 @@ public class CostModel {
             Statistics statistics = context.getStatistics();
             Preconditions.checkNotNull(statistics);
 
-            Statistics leftStatistics = context.getChildStatistics(0);
-            Statistics rightStatistics = context.getChildStatistics(1);
-
             List<BinaryPredicateOperator> eqOnPredicates =
-                    JoinHelper.getEqualsPredicate(leftStatistics.getUsedColumns(),
-                            rightStatistics.getUsedColumns(),
+                    JoinHelper.getEqualsPredicate(context.getChildOutputColumns(0),
+                            context.getChildOutputColumns(1),
                             Utils.extractConjuncts(join.getOnPredicate()));
 
             Preconditions.checkState(!(join.getJoinType().isCrossJoin() || eqOnPredicates.isEmpty()),
@@ -408,7 +405,6 @@ public class CostModel {
             double cpuCost = StatisticUtils.multiplyOutputSize(StatisticUtils.multiplyOutputSize(leftSize, rightSize),
                     EXECUTE_COST_PENALTY);
             double memCost = StatisticUtils.multiplyOutputSize(rightSize, EXECUTE_COST_PENALTY * 100D);
-
 
             if (join.getJoinType().isCrossJoin()) {
                 cpuCost = StatisticUtils.multiplyOutputSize(cpuCost, crossJoinCostPenalty);
