746c270928f9e4b5e41a2b0794ee03b5af8dc1a9
zihe.liu <ziheliu1024@gmail.com>
2025-10-13 17:38:49 +0800

    fix
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- be/src/exec/join/join_hash_map_helper.h
+++ be/src/exec/join/join_hash_map_helper.h
@@ -119,9 +119,9 @@ public:
         auto& data = down_cast<ColumnType*>(fixed_size_key_column)->get_data();
         auto* buf = reinterpret_cast<uint8_t*>(&data[start]);
 
-        std::memset(buf, 0, data.size() * count);
-
         constexpr size_t byte_interval = sizeof(CppType);
+        std::memset(buf, 0, count * byte_interval);
+
         size_t byte_offset = 0;
         for (uint32_t i = 0; i < key_columns.size(); i++) {
             const auto& key_col = key_columns[i];
