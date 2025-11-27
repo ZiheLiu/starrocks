06e4f4ceb9a2c078d9da9acf2577a57ecdf9dfb9
zihe.liu <ziheliu1024@gmail.com>
2025-11-27 15:42:21 +0800

    fix ctx.setExecutor(executor);
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/service/arrow/flight/sql/ArrowFlightSqlConnectProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/service/arrow/flight/sql/ArrowFlightSqlConnectProcessor.java
@@ -146,6 +146,7 @@ public class ArrowFlightSqlConnectProcessor extends ConnectProcessor {
 
         executor = new StmtExecutor(ctx, parsedStmt, deploymentFinished);
         ctx.setIsLastStmt(true);
+        ctx.setExecutor(executor);
 
         executor.addRunningQueryDetail(parsedStmt);
         executor.execute();
