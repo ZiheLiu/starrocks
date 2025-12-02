5fd62d53d7eb4b8a51c6e51ae80a737b9045d071
zihe.liu <ziheliu1024@gmail.com>
2025-12-02 19:39:19 +0800

    fix
    Signed-off-by: zihe.liu <ziheliu1024@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/service/arrow/flight/sql/ArrowFlightSqlConnectContext.java
+++ fe/fe-core/src/main/java/com/starrocks/service/arrow/flight/sql/ArrowFlightSqlConnectContext.java
@@ -146,7 +146,7 @@ public class ArrowFlightSqlConnectContext extends ConnectContext {
             String killSQL = "KILL " + getConnectionId();
             SimpleExecutor executor = new SimpleExecutor("ArrowFlightSQLCloseSession", TResultSinkType.MYSQL_PROTOCAL);
             try {
-                executor.executeDML(killSQL);
+                executor.executeControl(killSQL);
             } catch (Exception e) {
                 LOG.warn("Failed to kill the Arrow Flight SQL connection from the proxy to the leader.", e);
             }
