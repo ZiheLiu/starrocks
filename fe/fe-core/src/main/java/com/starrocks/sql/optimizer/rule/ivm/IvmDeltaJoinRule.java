b7a2c1e689f91df00bfc918b929be3e31d51e0d6
zihe.liu <ziheliu1024@gmail.com>
2026-03-04 20:54:56 +0800

    Refactor IVM delta join logic

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/ivm/IvmDeltaJoinRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rule/ivm/IvmDeltaJoinRule.java
@@ -15,23 +15,36 @@
 package com.starrocks.sql.optimizer.rule.ivm;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.starrocks.sql.ast.JoinOperator;
+import com.starrocks.sql.ast.expression.BinaryType;
+import com.starrocks.sql.optimizer.JoinHelper;
 import com.starrocks.sql.optimizer.OptExpression;
 import com.starrocks.sql.optimizer.OptimizerContext;
+import com.starrocks.sql.optimizer.Utils;
 import com.starrocks.sql.optimizer.base.ColumnRefFactory;
+import com.starrocks.sql.optimizer.base.ColumnRefSet;
 import com.starrocks.sql.optimizer.operator.OperatorType;
 import com.starrocks.sql.optimizer.operator.logical.LogicalDeltaOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalJoinOperator;
+import com.starrocks.sql.optimizer.operator.logical.LogicalProjectOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalUnionOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalVersionOperator;
 import com.starrocks.sql.optimizer.operator.pattern.Pattern;
+import com.starrocks.sql.optimizer.operator.scalar.BinaryPredicateOperator;
+import com.starrocks.sql.optimizer.operator.scalar.CaseWhenOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ColumnRefOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ConstantOperator;
+import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.rule.RuleType;
 import com.starrocks.sql.optimizer.rule.transformation.TransformationRule;
 import com.starrocks.sql.optimizer.rule.transformation.materialization.OptExpressionDuplicator;
+import com.starrocks.type.IntegerType;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class IvmDeltaJoinRule extends TransformationRule {
     public IvmDeltaJoinRule() {
@@ -45,84 +58,202 @@ public class IvmDeltaJoinRule extends TransformationRule {
         OptExpression joinExpr = input.inputAt(0);
         LogicalJoinOperator join = (LogicalJoinOperator) joinExpr.getOp();
         JoinOperator joinType = join.getJoinType();
-        if (!joinType.isInnerJoin() && !joinType.isCrossJoin()) {
+        if (!joinType.isInnerJoin() && !joinType.isCrossJoin() && !joinType.isLeftOuterJoin()) {
             return List.of();
         }
 
-        OptExpression leftChild = joinExpr.inputAt(0);
-        OptExpression rightChild = joinExpr.inputAt(1);
-        ColumnRefFactory factory = context.getColumnRefFactory();
-
-        List<ColumnRefOperator> joinOutputColumns =
-                joinExpr.getOutputColumns().getColumnRefOperators(context.getColumnRefFactory());
-        List<ColumnRefOperator> finalOutputColumns = Lists.newArrayList(joinOutputColumns);
+        ColumnRefFactory columnRefFactory = context.getColumnRefFactory();
         ColumnRefOperator actionColumn = delta.getActionColumn();
-        if (actionColumn != null) {
-            finalOutputColumns.add(actionColumn);
+        List<ColumnRefOperator> finalOutputColumns =
+                input.getOutputColumns().getColumnRefOperators(columnRefFactory);
+        List<ColumnRefOperator> joinOutputColumns = Lists.newArrayList();
+        for (ColumnRefOperator outputColumn : finalOutputColumns) {
+            if (actionColumn == null || outputColumn.getId() != actionColumn.getId()) {
+                joinOutputColumns.add(outputColumn);
+            }
         }
 
         List<OptExpression> unionChildren = Lists.newArrayList();
         List<List<ColumnRefOperator>> unionChildOutputs = Lists.newArrayList();
 
-        // delta(t1) INNER JOIN t2@from_version
-        BranchResult branch1 = buildBranch(factory, context, join, joinOutputColumns, actionColumn, leftChild, rightChild, true);
-        if (branch1 == null || branch1.outputs == null) {
+        // branch1: delta(t1) join t2@from_version
+        JoinOperator branch1JoinType = joinType.isLeftOuterJoin() ? JoinOperator.LEFT_OUTER_JOIN : joinType;
+        BranchResult branch1 = buildBranch(columnRefFactory, context, joinExpr, branch1JoinType, joinOutputColumns,
+                actionColumn, true);
+        if (!appendBranch(unionChildren, unionChildOutputs, branch1)) {
             return List.of();
         }
-        unionChildren.add(branch1.branchExpr);
-        unionChildOutputs.add(branch1.outputs);
 
-        // t1@to_version INNER JOIN delta(t2)
-        BranchResult branch2 = buildBranch(factory, context, join, joinOutputColumns, actionColumn, leftChild, rightChild, false);
-        if (branch2 == null || branch2.outputs == null) {
+        // branch2
+        BranchResult branch2;
+        if (joinType.isLeftOuterJoin()) {
+            branch2 = buildLeftOuterRightDeltaBranch(columnRefFactory, context, joinExpr, joinOutputColumns, actionColumn);
+        } else {
+            branch2 = buildBranch(columnRefFactory, context, joinExpr, joinType, joinOutputColumns, actionColumn, false);
+        }
+        if (!appendBranch(unionChildren, unionChildOutputs, branch2)) {
             return List.of();
         }
-        unionChildren.add(branch2.branchExpr);
-        unionChildOutputs.add(branch2.outputs);
 
         LogicalUnionOperator unionOperator = new LogicalUnionOperator(finalOutputColumns, unionChildOutputs, true);
         return List.of(OptExpression.create(unionOperator, unionChildren));
     }
 
-    private BranchInput duplicateChildren(ColumnRefFactory columnRefFactory,
-                                          OptimizerContext context,
-                                          OptExpression leftChild,
-                                          OptExpression rightChild) {
-        OptExpressionDuplicator leftDuplicator = new OptExpressionDuplicator(columnRefFactory, context);
-        OptExpressionDuplicator rightDuplicator = new OptExpressionDuplicator(columnRefFactory, context);
-        OptExpression newLeftChild = leftDuplicator.duplicate(leftChild);
-        OptExpression newRightChild = rightDuplicator.duplicate(rightChild);
-        return new BranchInput(newLeftChild, newRightChild,
-                leftDuplicator.getColumnMapping(), rightDuplicator.getColumnMapping());
+    private DuplicatedJoin duplicateJoin(ColumnRefFactory columnRefFactory,
+                                         OptimizerContext context,
+                                         OptExpression joinExpr) {
+        OptExpressionDuplicator duplicator = new OptExpressionDuplicator(columnRefFactory, context);
+        return new DuplicatedJoin(duplicator.duplicate(joinExpr), duplicator.getColumnMapping());
     }
 
     private BranchResult buildBranch(ColumnRefFactory columnRefFactory,
                                      OptimizerContext context,
-                                     LogicalJoinOperator join,
+                                     OptExpression joinExpr,
+                                     JoinOperator branchJoinType,
                                      List<ColumnRefOperator> joinOutputColumns,
                                      ColumnRefOperator actionColumn,
-                                     OptExpression leftChild,
-                                     OptExpression rightChild,
                                      boolean isLeftDelta) {
-        BranchInput branchInput = duplicateChildren(columnRefFactory, context, leftChild, rightChild);
+        DuplicatedJoin duplicatedJoin = duplicateJoin(columnRefFactory, context, joinExpr);
+        LogicalJoinOperator duplicatedJoinOp = (LogicalJoinOperator) duplicatedJoin.joinExpr().getOp();
         ColumnRefOperator branchActionColumn = duplicateActionColumn(columnRefFactory, actionColumn);
         OptExpression left;
         OptExpression right;
         if (isLeftDelta) {
-            left = OptExpression.create(new LogicalDeltaOperator(false, branchActionColumn), branchInput.leftChild);
-            right = OptExpression.create(LogicalVersionOperator.fromVersion(), branchInput.rightChild);
+            left = OptExpression.create(new LogicalDeltaOperator(false, branchActionColumn),
+                    duplicatedJoin.joinExpr().inputAt(0));
+            right = OptExpression.create(LogicalVersionOperator.fromVersion(), duplicatedJoin.joinExpr().inputAt(1));
         } else {
-            left = OptExpression.create(LogicalVersionOperator.toVersion(), branchInput.leftChild);
-            right = OptExpression.create(new LogicalDeltaOperator(false, branchActionColumn), branchInput.rightChild);
+            left = OptExpression.create(LogicalVersionOperator.toVersion(), duplicatedJoin.joinExpr().inputAt(0));
+            right = OptExpression.create(new LogicalDeltaOperator(false, branchActionColumn),
+                    duplicatedJoin.joinExpr().inputAt(1));
         }
 
-        List<ColumnRefOperator> outputs = deriveBranchOutputs(joinOutputColumns, branchActionColumn, branchInput);
+        List<ColumnRefOperator> outputs =
+                deriveBranchOutputs(joinOutputColumns, branchActionColumn, duplicatedJoin.columnMapping());
         if (outputs == null) {
             return null;
         }
 
-        LogicalJoinOperator newJoin = LogicalJoinOperator.builder().withOperator(join).build();
+        LogicalJoinOperator newJoin = LogicalJoinOperator.builder()
+                .withOperator(duplicatedJoinOp)
+                .setJoinType(branchJoinType)
+                .build();
         return new BranchResult(OptExpression.create(newJoin, left, right), outputs);
+    }
+
+    private BranchResult buildLeftOuterRightDeltaBranch(ColumnRefFactory columnRefFactory,
+                                                        OptimizerContext context,
+                                                        OptExpression joinExpr,
+                                                        List<ColumnRefOperator> joinOutputColumns,
+                                                        ColumnRefOperator actionColumn) {
+        if (actionColumn == null) {
+            return null;
+        }
+        LogicalJoinOperator join = (LogicalJoinOperator) joinExpr.getOp();
+
+        DuplicatedJoin positiveJoin = duplicateJoin(columnRefFactory, context, joinExpr);
+        DuplicatedJoin negativeJoin = duplicateJoin(columnRefFactory, context, joinExpr);
+        LogicalJoinOperator positiveJoinOp = (LogicalJoinOperator) positiveJoin.joinExpr().getOp();
+        ColumnRefOperator branchActionColumn = duplicateActionColumn(columnRefFactory, actionColumn);
+
+        OptExpression leftToVersion =
+                OptExpression.create(LogicalVersionOperator.toVersion(), positiveJoin.joinExpr().inputAt(0));
+        OptExpression rightDelta = OptExpression.create(new LogicalDeltaOperator(false, branchActionColumn),
+                positiveJoin.joinExpr().inputAt(1));
+        OptExpression negativeDelta = OptExpression.create(new LogicalDeltaOperator(false, branchActionColumn),
+                negativeJoin.joinExpr().inputAt(1));
+
+        List<ColumnRefOperator> originalRightOutputs =
+                joinExpr.inputAt(1).getOutputColumns().getColumnRefOperators(columnRefFactory);
+        Set<Integer> rightKeyColumnIds = deriveRightJoinKeyColumnIds(join, joinExpr.inputAt(0), joinExpr.inputAt(1));
+
+        Map<ColumnRefOperator, ScalarOperator> negativeProjectMap = Maps.newLinkedHashMap();
+        for (ColumnRefOperator originalRight : originalRightOutputs) {
+            ColumnRefOperator positiveRight = positiveJoin.columnMapping().get(originalRight);
+            ColumnRefOperator negativeRight = negativeJoin.columnMapping().get(originalRight);
+            if (positiveRight == null || negativeRight == null) {
+                return null;
+            }
+            if (rightKeyColumnIds.contains(originalRight.getId())) {
+                negativeProjectMap.put(positiveRight, negativeRight);
+            } else {
+                negativeProjectMap.put(positiveRight, ConstantOperator.createNull(positiveRight.getType()));
+            }
+        }
+        negativeProjectMap.put(branchActionColumn, buildReversedActionExpr(branchActionColumn));
+        OptExpression negativeProject = OptExpression.create(new LogicalProjectOperator(negativeProjectMap), negativeDelta);
+
+        List<ColumnRefOperator> rightUnionOutputs = Lists.newArrayListWithCapacity(originalRightOutputs.size() + 1);
+        for (ColumnRefOperator originalRight : originalRightOutputs) {
+            ColumnRefOperator positiveRight = positiveJoin.columnMapping().get(originalRight);
+            if (positiveRight == null) {
+                return null;
+            }
+            rightUnionOutputs.add(positiveRight);
+        }
+        rightUnionOutputs.add(branchActionColumn);
+        LogicalUnionOperator rightUnion = new LogicalUnionOperator(rightUnionOutputs,
+                List.of(rightUnionOutputs, rightUnionOutputs), true);
+        OptExpression rightAugmented = OptExpression.create(rightUnion, rightDelta, negativeProject);
+
+        List<ColumnRefOperator> outputs =
+                deriveBranchOutputs(joinOutputColumns, branchActionColumn, positiveJoin.columnMapping());
+        if (outputs == null) {
+            return null;
+        }
+
+        LogicalJoinOperator newJoin = LogicalJoinOperator.builder()
+                .withOperator(positiveJoinOp)
+                .setJoinType(JoinOperator.INNER_JOIN)
+                .build();
+        return new BranchResult(OptExpression.create(newJoin, leftToVersion, rightAugmented), outputs);
+    }
+
+    private boolean appendBranch(List<OptExpression> unionChildren,
+                                 List<List<ColumnRefOperator>> unionChildOutputs,
+                                 BranchResult branch) {
+        if (branch == null || branch.outputs() == null) {
+            return false;
+        }
+        unionChildren.add(branch.branchExpr());
+        unionChildOutputs.add(branch.outputs());
+        return true;
+    }
+
+    private Set<Integer> deriveRightJoinKeyColumnIds(LogicalJoinOperator join,
+                                                     OptExpression leftChild,
+                                                     OptExpression rightChild) {
+        Set<Integer> rightKeyColumnIds = new HashSet<>();
+        ScalarOperator onPredicate = join.getOnPredicate();
+        if (onPredicate == null) {
+            return rightKeyColumnIds;
+        }
+        ColumnRefSet leftColumns = leftChild.getOutputColumns();
+        ColumnRefSet rightColumns = rightChild.getOutputColumns();
+        List<BinaryPredicateOperator> eqPredicates =
+                JoinHelper.getEqualsPredicate(leftColumns, rightColumns, Utils.extractConjuncts(onPredicate));
+        for (BinaryPredicateOperator eqPredicate : eqPredicates) {
+            ColumnRefSet lhsUsed = eqPredicate.getChild(0).getUsedColumns();
+            ColumnRefSet rhsUsed = eqPredicate.getChild(1).getUsedColumns();
+            if (rightColumns.containsAll(lhsUsed)) {
+                for (int id : lhsUsed.getColumnIds()) {
+                    rightKeyColumnIds.add(id);
+                }
+            } else if (rightColumns.containsAll(rhsUsed)) {
+                for (int id : rhsUsed.getColumnIds()) {
+                    rightKeyColumnIds.add(id);
+                }
+            }
+        }
+        return rightKeyColumnIds;
+    }
+
+    private ScalarOperator buildReversedActionExpr(ColumnRefOperator actionColumn) {
+        ScalarOperator isDeleteAction = new BinaryPredicateOperator(BinaryType.LT, actionColumn,
+                ConstantOperator.createTinyInt((byte) 0));
+        return new CaseWhenOperator(IntegerType.TINYINT, null,
+                ConstantOperator.createTinyInt((byte) -1),
+                List.of(isDeleteAction, ConstantOperator.createTinyInt((byte) 1)));
     }
 
     private ColumnRefOperator duplicateActionColumn(ColumnRefFactory columnRefFactory, ColumnRefOperator actionColumn) {
@@ -134,14 +265,11 @@ public class IvmDeltaJoinRule extends TransformationRule {
 
     private List<ColumnRefOperator> deriveBranchOutputs(List<ColumnRefOperator> joinOutputColumns,
                                                         ColumnRefOperator actionColumn,
-                                                        BranchInput branch) {
+                                                        Map<ColumnRefOperator, ColumnRefOperator> oldToNewColumnMapping) {
         List<ColumnRefOperator> outputs =
                 Lists.newArrayListWithCapacity(joinOutputColumns.size() + (actionColumn == null ? 0 : 1));
         for (ColumnRefOperator output : joinOutputColumns) {
-            ColumnRefOperator mappedOutput = branch.leftOldToNew.get(output);
-            if (mappedOutput == null) {
-                mappedOutput = branch.rightOldToNew.get(output);
-            }
+            ColumnRefOperator mappedOutput = oldToNewColumnMapping.get(output);
             if (mappedOutput == null) {
                 return null;
             }
@@ -153,12 +281,9 @@ public class IvmDeltaJoinRule extends TransformationRule {
         return outputs;
     }
 
-    private record BranchInput(OptExpression leftChild, OptExpression rightChild,
-                               Map<ColumnRefOperator, ColumnRefOperator> leftOldToNew,
-                               Map<ColumnRefOperator, ColumnRefOperator> rightOldToNew) {
+    private record DuplicatedJoin(OptExpression joinExpr, Map<ColumnRefOperator, ColumnRefOperator> columnMapping) {
     }
 
     private record BranchResult(OptExpression branchExpr, List<ColumnRefOperator> outputs) {
     }
-
 }
--- fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/ivm/IvmDeltaJoinRuleTest.java
+++ fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/ivm/IvmDeltaJoinRuleTest.java
@@ -26,7 +26,6 @@ import com.starrocks.sql.optimizer.base.ColumnRefFactory;
 import com.starrocks.sql.optimizer.operator.logical.LogicalDeltaOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalJoinOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalOlapScanOperator;
-import com.starrocks.sql.optimizer.operator.logical.LogicalProjectOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalUnionOperator;
 import com.starrocks.sql.optimizer.operator.logical.LogicalVersionOperator;
 import com.starrocks.sql.optimizer.operator.scalar.BinaryPredicateOperator;
@@ -99,8 +98,7 @@ public class IvmDeltaJoinRuleTest {
         Assertions.assertTrue(union.getOutputColumnRefOp().contains(actionRef));
 
         // Branch 1: delta(left) join version(right, right_from=2)
-        Assertions.assertTrue(rewritten.inputAt(0).getOp() instanceof LogicalProjectOperator);
-        OptExpression firstJoin = rewritten.inputAt(0).inputAt(0);
+        OptExpression firstJoin = rewritten.inputAt(0);
         Assertions.assertTrue(firstJoin.getOp() instanceof LogicalJoinOperator);
         Assertions.assertTrue(firstJoin.inputAt(0).getOp() instanceof LogicalDeltaOperator);
         Assertions.assertTrue(firstJoin.inputAt(1).getOp() instanceof LogicalVersionOperator);
@@ -108,13 +106,86 @@ public class IvmDeltaJoinRuleTest {
                 ((LogicalVersionOperator) firstJoin.inputAt(1).getOp()).getVersionRefType());
 
         // Branch 2: version(left, left_to=3) join delta(right)
-        Assertions.assertTrue(rewritten.inputAt(1).getOp() instanceof LogicalProjectOperator);
-        OptExpression secondJoin = rewritten.inputAt(1).inputAt(0);
+        OptExpression secondJoin = rewritten.inputAt(1);
         Assertions.assertTrue(secondJoin.getOp() instanceof LogicalJoinOperator);
         Assertions.assertTrue(secondJoin.inputAt(0).getOp() instanceof LogicalVersionOperator);
         Assertions.assertEquals(LogicalVersionOperator.VersionRefType.TO_VERSION,
                 ((LogicalVersionOperator) secondJoin.inputAt(0).getOp()).getVersionRefType());
         Assertions.assertTrue(secondJoin.inputAt(1).getOp() instanceof LogicalDeltaOperator);
+    }
+
+    @Test
+    public void testTransformLeftOuterJoin(@Mocked OlapTable leftTable,
+                                           @Mocked OlapTable rightTable) {
+        new Expectations() {
+            {
+                leftTable.getBaseIndexMetaId();
+                result = 1L;
+                rightTable.getBaseIndexMetaId();
+                result = 1L;
+            }
+        };
+
+        ColumnRefFactory columnRefFactory = new ColumnRefFactory();
+        OptimizerContext context = OptimizerFactory.mockContext(columnRefFactory);
+        ColumnRefOperator leftRef = columnRefFactory.create("l_id", IntegerType.INT, false);
+        ColumnRefOperator rightRef = columnRefFactory.create("r_id", IntegerType.INT, false);
+        ColumnRefOperator rightValueRef = columnRefFactory.create("r_v", IntegerType.INT, true);
+        ColumnRefOperator actionRef = columnRefFactory.create("__op", IntegerType.TINYINT, false);
+
+        Column leftColumn = new Column("l_id", IntegerType.INT, false);
+        Column rightColumn = new Column("r_id", IntegerType.INT, false);
+        Column rightValueColumn = new Column("r_v", IntegerType.INT, true);
+        LogicalOlapScanOperator leftScan = LogicalOlapScanOperator.builder()
+                .withOperator(new LogicalOlapScanOperator(leftTable,
+                        Maps.newHashMap(Map.of(leftRef, leftColumn)),
+                        Maps.newHashMap(Map.of(leftColumn, leftRef)),
+                        null,
+                        -1,
+                        null))
+                .setTableVersion(1L)
+                .build();
+        LogicalOlapScanOperator rightScan = LogicalOlapScanOperator.builder()
+                .withOperator(new LogicalOlapScanOperator(rightTable,
+                        Maps.newHashMap(Map.of(rightRef, rightColumn, rightValueRef, rightValueColumn)),
+                        Maps.newHashMap(Map.of(rightColumn, rightRef, rightValueColumn, rightValueRef)),
+                        null,
+                        -1,
+                        null))
+                .setTableVersion(2L)
+                .build();
+
+        OptExpression leftExpr = OptExpression.create(leftScan);
+        OptExpression rightExpr = OptExpression.create(rightScan);
+        BinaryPredicateOperator onPredicate = new BinaryPredicateOperator(BinaryType.EQ, leftRef, rightRef);
+        OptExpression joinExpr = OptExpression.create(new LogicalJoinOperator(JoinOperator.LEFT_OUTER_JOIN, onPredicate),
+                leftExpr, rightExpr);
+        OptExpression deltaJoinExpr = OptExpression.create(new LogicalDeltaOperator(true, actionRef), joinExpr);
+        deriveLogicalProperty(deltaJoinExpr);
+
+        List<OptExpression> result = new IvmDeltaJoinRule().transform(deltaJoinExpr, context);
+        Assertions.assertEquals(1, result.size());
+
+        OptExpression rewritten = result.get(0);
+        Assertions.assertTrue(rewritten.getOp() instanceof LogicalUnionOperator);
+        Assertions.assertEquals(2, rewritten.arity());
+
+        OptExpression firstJoin = rewritten.inputAt(0);
+        Assertions.assertTrue(firstJoin.getOp() instanceof LogicalJoinOperator);
+        Assertions.assertEquals(JoinOperator.LEFT_OUTER_JOIN, ((LogicalJoinOperator) firstJoin.getOp()).getJoinType());
+        Assertions.assertTrue(firstJoin.inputAt(0).getOp() instanceof LogicalDeltaOperator);
+        Assertions.assertTrue(firstJoin.inputAt(1).getOp() instanceof LogicalVersionOperator);
+        Assertions.assertEquals(LogicalVersionOperator.VersionRefType.FROM_VERSION,
+                ((LogicalVersionOperator) firstJoin.inputAt(1).getOp()).getVersionRefType());
+
+        OptExpression secondJoin = rewritten.inputAt(1);
+        Assertions.assertTrue(secondJoin.getOp() instanceof LogicalJoinOperator);
+        Assertions.assertEquals(JoinOperator.INNER_JOIN, ((LogicalJoinOperator) secondJoin.getOp()).getJoinType());
+        Assertions.assertTrue(secondJoin.inputAt(0).getOp() instanceof LogicalVersionOperator);
+        Assertions.assertEquals(LogicalVersionOperator.VersionRefType.TO_VERSION,
+                ((LogicalVersionOperator) secondJoin.inputAt(0).getOp()).getVersionRefType());
+        Assertions.assertTrue(secondJoin.inputAt(1).getOp() instanceof LogicalUnionOperator);
+        Assertions.assertEquals(2, secondJoin.inputAt(1).arity());
     }
 
     private static void deriveLogicalProperty(OptExpression expression) {
